0fef446a6f58adc2da45eb98132774aa44637d5a
樂樂 <yile365@sina.com>
2020-02-23 15:51:33 +0800

    ''

--- script/baidu.ad.user.js
+++ script/baidu.ad.user.js
@@ -19,30 +19,25 @@ $(function() {
 
 function removeAd(){
     count++;
-	printLog("count"+count);
     if(count>=100){
         return;
     }
 	tryCatch(function(){
-		printLog("::shadow div:"+$('::shadow div').length);
 		if($('::shadow div').length>0){
 			$('::shadow div').remove();
 		}
 	});
 	tryCatch(function(){
-		printLog(".blank-frame"+$('.blank-frame').length);
 		if($(".blank-frame").length>0){
 			$(".blank-frame").remove();
 		}
 	});
 	tryCatch(function(){
-		printLog("#copyright.next"+$("#copyright").next().length);
 		if($("#copyright").next().length>0){
 			$("#copyright").next().remove();
 		}
 	});
 	tryCatch(function(){
-		printLog("section.hint-fold-results-wrapper.hint-no-fold"+$('.blank-frame').length);
 		if($("section.hint-fold-results-wrapper.hint-no-fold").length>0){
 			$("section.hint-fold-results-wrapper.hint-no-fold").remove();
 		}
@@ -58,7 +53,6 @@ function removeAd(){
 		}
 	});
 	tryCatch(function(){
-		printLog(".hint-fold-results-box--length"+$(".hint-fold-results-box").length);
 		if($(".hint-fold-results-box").length>0){
 			$(".hint-fold-results-box").remove();
 			$("#page-relative").css("display","inline");
@@ -66,9 +60,13 @@ function removeAd(){
 		}
 	});
 	tryCatch(function(){
-		printLog(".tab-news-content--length"+$(".tab-news-content").length);
 		if($(".tab-news-content").length>0){
 			$(".tab-news-content").closest(".blank-frame").remove();
+		}
+	});
+	tryCatch(function(){
+		if($(".ec_wise_ad").length>0){
+			$(".ec_wise_ad").remove();
 		}
 	});
     setTimeout(removeAd,100);
